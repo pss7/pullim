f44396edbd8742c9781e507cce6b9be27d3199aa
parksangseok <park0157@naver.com>
2026-08-21 11:59:58 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,69 @@
 $(function () {
 
+    /* 새로고침 시 상단 */
+    if ('scrollRestoration' in history) {
+        history.scrollRestoration = 'manual';
+    }
 
-  /* 새로고침 시 상단 */
-  if ('scrollRestoration' in history) {
-    history.scrollRestoration = 'manual';
-  }
+    window.scrollTo(0, 0);
 
-  window.scrollTo(0, 0);
 
-  /* LENIS */
-  const lenis = new Lenis({
-    duration: 1.2,
-    smoothWheel: true,
-    autoRaf: true
-  });
+    /* LENIS */
+    const lenis = new Lenis({
+        duration: 1.2,
+        smoothWheel: true,
+        autoRaf: true
+    });
+
+
+    /* VISUAL */
+    const visualWrap = document.querySelector('#visualWrap');
+    const visualBox = document.querySelector('#visualWrap .visualBox');
+
+
+    /* =========================
+       페이지 진입
+       텍스트 등장
+    ========================= */
+
+    setTimeout(function () {
+
+        visualBox.classList.add('active');
+
+    }, 100);
+
+
+    /* =========================
+       스크롤
+    ========================= */
+
+    lenis.on('scroll', function () {
+
+        const scrollTop = lenis.scroll;
+
+        const visualTop = visualWrap.offsetTop;
+
+
+        /*
+         * visualWrap에 진입한 순간
+         */
+        const progress = scrollTop - visualTop;
+
+
+        /*
+         * visualWrap 시작점에서
+         * 조금만 스크롤하면 step01
+         */
+        if (progress > 50) {
+
+            visualBox.classList.add('step01');
+
+        } else {
+
+            visualBox.classList.remove('step01');
+
+        }
+
+    });
 
 });
